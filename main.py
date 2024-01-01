6c9e17ef6439adae81dc3b1d4c617968bf7197ea
Adam Olczak <adam@olczak.xyz>
2024-01-02 00:36:40 +0100

    Pass in the book filename as parameter
    Allow to specify any text file in the books/ directory as an argument to the script.
    Add usage instructions if the flename is not provided.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import sys
+
 from book import report
 
 
 def main():
-    report("frankenstein.txt")
+    if len(sys.argv) < 2:
+        usage()
+        sys.exit(1)
+    filename = sys.argv[1]
+    report(filename)
+
+
+def usage():
+    print(f"Usage: {sys.argv[0]} filename")
+    print()
+    print("Arguments:")
+    print("\tfilename - filename of the book in books/ directory")
 
 
 if __name__ == "__main__":
